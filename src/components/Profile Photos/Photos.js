5cbfa46a689a69e9a2005b54cdeb8330098f02a2
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-02 12:38:29 +0300

    Photos component

--- src/components/Profile Photos/Photos.js
+++ src/components/Profile Photos/Photos.js
@@ -1,179 +1,179 @@
-// export function Photos() {
-//     return (
-//         <div className="col-lg-6">
-//             <div className="central-meta">
-//                 <ul className="photos">
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-22.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo2.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-33.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo3.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-44.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo4.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-55.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo5.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-66.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo6.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-77.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo7.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-88.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo8.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-99.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo12.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-101.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo10.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-101.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo11.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-22.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo1.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-33.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo9.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-99.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo12.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-66.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo6.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                     <li>
-//                         <a
-//                             className="strip"
-//                             href="images/resources/photo-66.jpg"
-//                             title=""
-//                             data-strip-group="mygroup"
-//                             data-strip-group-options="loop: false"
-//                         >
-//                             <img src="images/resources/photo13.jpg" alt="" />
-//                         </a>
-//                     </li>
-//                 </ul>
-//                 <div className="lodmore">
-//                     <button className="btn-view btn-load-more" />
-//                 </div>
-//             </div>
-//             {/* photos */}
-//         </div>
-//     );
-// };
+export function Photos() {
+    return (
+        <div className="col-lg-6">
+            <div className="central-meta">
+                <ul className="photos">
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-22.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo2.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-33.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo3.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-44.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo4.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-55.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo5.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-66.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo6.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-77.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo7.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-88.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo8.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-99.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo12.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-101.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo10.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-101.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo11.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-22.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo1.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-33.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo9.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-99.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo12.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-66.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo6.jpg" alt="" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            className="strip"
+                            href="images/resources/photo-66.jpg"
+                            title=""
+                            data-strip-group="mygroup"
+                            data-strip-group-options="loop: false"
+                        >
+                            <img src="images/resources/photo13.jpg" alt="" />
+                        </a>
+                    </li>
+                </ul>
+                <div className="lodmore">
+                    <button className="btn-view btn-load-more" />
+                </div>
+            </div>
+            {/* photos */}
+        </div>
+    );
+};
