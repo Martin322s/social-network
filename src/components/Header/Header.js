e268a200705ccfba6d00b1786def5fe8a82e1134
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-03 15:04:38 +0300

    a tag prevent refresh

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
     return (
         <header>
@@ -11,17 +13,14 @@ export function Header() {
                         </div>
                         <div className="top-area">
                             <ul className="main-menu">
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/">Timeline</a></li>
-                                <li><a href="/">Login</a></li>
-                                <li><a href="/">Sign In</a></li>
-                                <li><a href="/">Logout</a></li>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/">Timeline</Link></li>
+                                <li><Link to="/users/login">Login</Link></li>
+                                <li><Link to="/users/register">Sign In</Link></li>
+                                <li><Link to="/users/logout">Logout</Link></li>
                             </ul>
                             <ul className="setting-area">
                                 <li>
-                                    <a href="/" title="Home" data-ripple="">
-                                        <i className="ti-search" />
-                                    </a>
                                     <div className="searched">
                                         <form method="post" className="form-search">
                                             <input type="text" placeholder="Search" />
