734df33f43ec4a90e842be8bbcf4ea6f0b1c5c4d
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-18 13:21:53 +0300

    search form name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,16 +8,20 @@ export function Header() {
                 <div className="theme-layout">
                     <div className="topbar stick">
                         <div className="logo">
-                            <a href="/">
+                            <Link to="/">
                                 <img src="/images/logo.png" alt="logo" />
-                            </a>
+                            </Link>
                         </div>
                         <div className="top-area">
                             <ul className="setting-area">
                                 <li>
                                     <div className="searched">
-                                        <form method="post" className="form-search">
-                                            <input type="text" placeholder="Search" />
+                                        <form className="form-search">
+                                            <input 
+                                                type="text"
+                                                placeholder="Search"
+                                                name="search"
+                                            />
                                             <button data-ripple="">
                                                 <i className="ti-search" />
                                             </button>
