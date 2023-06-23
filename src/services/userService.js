7d26c18cafee78548b648b1e57cc1934a678fb4e
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-23 20:16:36 +0300

    user data validation

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,23 @@
 const baseUrl = "http://localhost:3030/users";
 
 export const registerUser = async (userData) => {
-    const res = await fetch(`${baseUrl}/regitser`);
-    const data = await res.json();
-    return data;
+    try {
+        if (Object.values(userData).some(x => x === "")) {
+            throw {
+                message: "Invalid data provided!"             
+            }
+        }
+
+        const res = await fetch(`${baseUrl}/regitser`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+        const data = await res.json();
+        return data;
+    } catch (err) {
+
+    }
 }
