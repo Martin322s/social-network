4c819e5958369ce31c3740109cd30c9c582e0b10
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-06-27 09:56:29 +0300

    user / guest navigation

--- src/components/Shortcuts/Shortcuts.js
+++ src/components/Shortcuts/Shortcuts.js
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { AuthContext } from "../../contexts/AuthContext";
 export function Shortcuts() {
+    const { user } = useContext(AuthContext);
+
     return (
         <aside className="sidebar static shortcuts">
             <div className="widget">
@@ -10,38 +13,45 @@ export function Shortcuts() {
                         <i className="ti-mouse-alt" />
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
-                        <i className="ti-user" />
-                        <Link to="/profile">Profile</Link>
-                    </li>
-                    <li>
-                        <i className="ti-user" />
-                        <Link to="/profile/messages-inbox">Messages</Link>
-                    </li>
-                    <li>
-                        <i className="ti-image" />
-                        <Link to="/profile/images">Images</Link>
-                    </li>
-                    <li>
-                        <i className="ti-video-camera" />
-                        <Link to="/profile/timeline">Timeline</Link>
-                    </li>
-                    <li>
-                        <i className="ti-video-camera" />
-                        <Link to="/contacts">Contacts</Link>
-                    </li>
-                    <li>
-                        <i className="fas fa-door-open" />
-                        <Link to="/users/login">Login</Link>
-                    </li>
-                    <li>
-                        <i className="fas fa-user-plus" />
-                        <Link to="/users/register">Register</Link>
-                    </li>
-                    <li>
-                        <i className="ti-power-off" />
-                        <Link to="/users/logout">Logout</Link>
-                    </li>
+                    {user.accessToken ?
+                        <>
+                            <li>
+                                <i className="ti-user" />
+                                <Link to="/profile">Profile</Link>
+                            </li>
+                            <li>
+                                <i className="ti-user" />
+                                <Link to="/profile/messages-inbox">Messages</Link>
+                            </li>
+                            <li>
+                                <i className="ti-image" />
+                                <Link to="/profile/images">Images</Link>
+                            </li>
+                            <li>
+                                <i className="ti-power-off" />
+                                <Link to="/users/logout">Logout</Link>
+                            </li>
+                        </>
+                        :
+                        <>
+                            <li>
+                                <i className="ti-video-camera" />
+                                <Link to="/profile/timeline">Timeline</Link>
+                            </li>
+                            <li>
+                                <i className="ti-video-camera" />
+                                <Link to="/contacts">Contacts</Link>
+                            </li>
+                            <li>
+                                <i className="fas fa-door-open" />
+                                <Link to="/users/login">Login</Link>
+                            </li>
+                            <li>
+                                <i className="fas fa-user-plus" />
+                                <Link to="/users/register">Register</Link>
+                            </li>
+                        </>
+                    }
                 </ul>
             </div>
         </aside>
