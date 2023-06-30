3d62b74d4282efb1b0a3729e6ace35329bc7feaf
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-01 00:44:20 +0300

    login user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -21,3 +21,25 @@ export const registerUser = async (userData) => {
         return err;
     }
 }
+
+export const loginUser = async (userData) => {
+    try {
+        if (Object.values(userData).some(x => x === "")) {
+            throw new Error ({ message: "Invalid data provided!" });
+        } else if (userData.password.length < 6) {
+            throw new Error ({ message: "Password should be at least 6 characters long!" });
+        } else {
+            const res = await fetch(`${baseUrl}/login`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+
+            return await res.json();
+        }
+    } catch (err) {
+        return err;
+    }
+}
