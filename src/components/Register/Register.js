161ec69b78b28857970d36ff375faa2c7682d62c
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-02 12:42:37 +0300

    logout service call

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ export function Register() {
             userService.registerUser(data)
                 .then((result) => {
                     userLogin(result);
-                    navigate('/');
+                    navigate('/', { replace: true });
                 });
         }
     }
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,9 +3,9 @@ const baseUrl = "http://localhost:3030/users";
 export const registerUser = async (userData) => {
     try {
         if (Object.values(userData).some(x => x === "")) {
-            throw new Error ({ message: "Invalid data provided!" });
+            throw new Error({ message: "Invalid data provided!" });
         } else if (userData.password.length < 6) {
-            throw new Error ({ message: "Password should be at least 6 characters long!" });
+            throw new Error({ message: "Password should be at least 6 characters long!" });
         } else {
             const res = await fetch(`${baseUrl}/register`, {
                 method: "POST",
@@ -25,9 +25,9 @@ export const registerUser = async (userData) => {
 export const loginUser = async (userData) => {
     try {
         if (Object.values(userData).some(x => x === "")) {
-            throw new Error ({ message: "Invalid data provided!" });
+            throw new Error({ message: "Invalid data provided!" });
         } else if (userData.password.length < 6) {
-            throw new Error ({ message: "Password should be at least 6 characters long!" });
+            throw new Error({ message: "Password should be at least 6 characters long!" });
         } else {
             const res = await fetch(`${baseUrl}/login`, {
                 method: "POST",
@@ -43,3 +43,10 @@ export const loginUser = async (userData) => {
         return err;
     }
 }
+
+export const logoutUser = (token) => fetch(`${baseUrl}/logout`, {
+    method: "GET",
+    headers: {
+        'X-Authorization': token
+    }
+});
