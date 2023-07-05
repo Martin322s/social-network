8d1c6ace5e1baa8577ee048b5deaa3e087517f48
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-05 18:41:56 +0300

    clear unused functions

--- src/components/Edit Profile/EditProfile.js
+++ src/components/Edit Profile/EditProfile.js
@@ -25,8 +25,6 @@ export function EditProfile() {
     const submitHandler = (ev, data) => {
         ev.preventDefault();
 
-        console.log(data);
-
         userService.updateUserInfo(user._id, data, user.accessToken)
             .then(() => navigate('/profile', { replace: true }));
     }
