c06a5ae33a4f92605e623b66e7842daf93fb2707
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-01 18:29:23 +0200

    create vite-app

--- src/components/Login/utils/utils.js
+++
@@ -1,3 +0,0 @@
-export const changeHandler = (ev, dispatch) => {
-    dispatch({ type: "SET_FIELD", field: ev.target.name, value: ev.target.value });
-}
--- src/components/Register/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const changeHandler = (ev, dispatch) => {
-    dispatch({ type: "SET_FIELD", field: ev.target.name, value: ev.target.value });
-}
-
-export const termsStatusChange = (ev, dispatch) => {
-    dispatch({ type: "TAC_CHECK", field: ev.target.name, value: ev.target.checked });
-}
