1f82dcee720b88199b4b1bce99ac25f51506ed67
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-02 10:35:02 +0200

    separate application to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import Navigation from "./components/Navigation/Navigation";
+import Rightbar from "./components/Rightbar/Rightbar";
+import Profile from "./components/Profile/Profile"
+import RecentChats from "./components/Recent Chats/RescentChats";
+import PhoneCalls from "./components/PhoneCalls/PhoneCalls";
+import TabPanel from "./components/TabPanel/TabPanel";
+import ChatBody from "./components/Chat Body/ChatBody";
+import InviteNewFriends from "./components/Invite New Friends/InviteNewFriends";
+import Options from "./components/Options/Options";
 
 function App() {
 
     return (
-        <>
+        <div id="layout" className="theme-cyan">
             <Navigation />
-        </>
-    )
+            <Rightbar />
+            <div className="sidebar border-end py-xl-4 py-3 px-xl-4 px-3">
+                <div className="tab-content">
+                    <Profile />
+                    <RecentChats />
+                    <PhoneCalls />
+                    <TabPanel />
+                    <InviteNewFriends />
+                    <Options />
+                    <ChatBody />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
